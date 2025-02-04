6bb8df48a50caafd6b102adb39bcbef89787b363
Nick Efimov <lamoz760@gmail.com>
2025-02-04 16:29:31 +0300

    add param for nuxt 3 ssr

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ export default defineNuxtConfig({
     compatibilityDate: "2024-11-01",
     devtools: { enabled: true },
     // target: "static", // Генерируем статический сайт
+    ssr: false, // Отключаем SSR для статической генерации
     app: {
         baseURL: "/test2/", // Укажите название репозитория
     },
