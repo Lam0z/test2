f862714a52c8bb889a9d72b50443d95f87dabe16
Nick Efimov <lamoz760@gmail.com>
2025-02-04 16:24:01 +0300

    deploy settings

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
-})
+    compatibilityDate: "2024-11-01",
+    devtools: { enabled: true },
+    // target: "static", // Генерируем статический сайт
+    app: {
+        baseURL: "/test2/", // Укажите название репозитория
+    },
+});
