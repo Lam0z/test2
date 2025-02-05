1aa9bba0f5c50f541962b004eb69e425e3832b0c
Nick Efimov <lamoz760@gmail.com>
2025-02-05 14:37:45 +0300

    add yaml

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     compatibilityDate: "2024-11-01",
-    // nitro: {
-    //     prerender: {
-    //         routes: ["/"], // Указываем страницы для статической генерации
-    //     },
-    // },
-    // app: {
-    //     baseURL: "/test2/", // Укажите название репозитория
-    // },
-    // devtools: { enabled: true },
-
+    ssr: false, // Отключаем SSR для статической генерации
+    app: {
+        baseURL: "/test2/", // Укажите имя вашего репозитория
+        buildAssetsDir: "assets", // Папка для статических файлов
+    },
     modules: ["@vueuse/nuxt", "@nuxt/fonts"],
     fonts: {
         families: [{ name: "Raleway", provider: "google" }],
