594a126ec8cc4c1fcf75a2ff218ee9231f1c1b51
Nick Efimov <lamoz760@gmail.com>
2025-02-05 15:45:38 +0300

    change path

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+const baseURL = "/test2/";
 export default defineNuxtConfig({
     compatibilityDate: "2024-11-01",
     ssr: false, // Отключаем SSR для статической генерации
     app: {
-        baseURL: "/test2/", // Укажите имя вашего репозитория
+        baseURL: baseURL, // Укажите имя вашего репозитория
         buildAssetsDir: "assets", // Папка для статических файлов
         head: {
             link: [
-                { rel: "icon", type: "image/x-icon", href: "/favicon.ico" }, // путь к favicon
+                {
+                    rel: "icon",
+                    type: "image/x-icon",
+                    href: `${baseURL}/favicon.ico`,
+                }, // путь к favicon
             ],
         },
     },
